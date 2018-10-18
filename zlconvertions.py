0e6e1aa0d2bc5e324abdc446baad5b0cdd5fae8c
leizhao2018 <43003916+leizhao2018@users.noreply.github.com>
2018-10-18 09:39:58 -0400

    Update zlconvertions.py

--- zlconvertions.py
+++ zlconvertions.py
@@ -194,43 +194,6 @@ def list_all_files(rootdir):
            if os.path.isfile(path):
               _files.append(path)
     return _files
-
-    
-#def partial_ratio(s1, s2):
-#    """"Return the ratio of the most similar substring
-#    as a number between 0 and 100."""
-#    s1, s2 = utils.make_type_consistent(s1, s2)
-#
-#    if len(s1) <= len(s2):
-#        shorter = s1
-#        longer = s2
-#    else:
-#        shorter = s2
-#        longer = s1
-#
-#    m = SequenceMatcher(None, shorter, longer)
-#    blocks = m.get_matching_blocks()
-#
-#    # each block represents a sequence of matching characters in a string
-#    # of the form (idx_1, idx_2, len)
-#    # the best partial match will block align with at least one of those blocks
-#    #   e.g. shorter = "abcd", longer = XXXbcdeEEE
-#    #   block = (1,3,3)
-#    #   best score === ratio("abcd", "Xbcd")
-#    scores = []
-#    for block in blocks:
-#        long_start = block[1] - block[0] if (block[1] - block[0]) > 0 else 0
-#        long_end = long_start + len(shorter)
-#        long_substr = longer[long_start:long_end]
-#
-#        m2 = SequenceMatcher(None, shorter, long_substr)
-#        r = m2.ratio()
-#        if r > .995:
-#            return 100
-#        else:
-#            scores.append(r)
-#
-#    return utils.intr(100 * max(scores))
     
 """
 sd2uv:    21
@@ -239,20 +202,3 @@ list_sd2uv(s,d):  58
 list_uv2sd:   67
 sd_list_mean:    75
 """
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
