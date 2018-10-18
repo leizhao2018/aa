d5be2891afc39321dbec6c79633197cc38a0fa32
leizhao2018 <43003916+leizhao2018@users.noreply.github.com>
2018-10-18 09:34:57 -0400

    Update check_data.py

--- check_data.py
+++ check_data.py
@@ -11,40 +11,57 @@ modefied in Oct 3,2018
 """
 import pandas as pd
 import os
-import glob
+import zlconvertions as zl
 
 #HARDCODES
 mindepth=10 #minimum depth (meters) that a file must have to be considered usable
-input_dir='/home/jmanning/leizhao/data_file/'  #the directory is the boat data folder and the file that contact name with vessel folder 
-input_dir2='input_data/boat_data_needs_upload/data_on_Lisa_ann_3/towifi/' #the direction of the boat data
-vessel_number_file='vessel_number.txt'      #name contact vessel number file this file has vessel_# and vessel name colums
-vessel_name='Lisa_Ann_III'          # the data from whose boat
+input_dir='/home/jmanning/leizhao/data_file/input_data/check_data/test'  #the directory is the boat data folder
+output_dir='/home/jmanning/leizhao/data_file/output_data/check_data/test/test' #the directory is the path we need save the checed data 
+vessel_number_path_file='/home/jmanning/leizhao/data_file/vessel_number.txt'      #this file contact vessel number and vessel name
 Lowell_SN_2='7a'        #the first two letters of Lowell_SN
 ##################################
 
 print 'the program is running'
-vessel_number_df=pd.read_csv(input_dir+vessel_number_file,names=['name','vessel_number']) 
-#get the files path and name
-file_lists=glob.glob(os.path.join(input_dir+input_dir2,'*.csv'))
+#read the file of the vessel_number
+vessel_number_df=pd.read_csv(vessel_number_path_file,names=['name','vessel_number']) 
+#get all the files under the input folder
+allfile_lists=zl.list_all_files(input_dir)
+
+# produce a dataframe that use to caculate the number of items
+total_df=vessel_number_df
+total_df.insert(2,'file_total',0)
+total_df=total_df.drop(['vessel_number'],axis=1)
+total_df['name'][0]='Total'
+
+#screen out the file of '.csv',and put the path+name in the fil_lists
+file_lists=[]
+for file in allfile_lists:
+    if file[len(file)-4:]=='.csv':
+        file_lists.append(file)
+
+#start check the data and save in the output_dir
 for file in file_lists:
+    fpath,fname=os.path.split(file)  #get the file's path and name
     
     #fix the file name
-    if len(file.split('/')[len(file.split('/'))-1].split('_')[1])==2:# if the serieal number is only 2 digits make it 4
-        new_file=file.split('/')[len(file.split('/'))-1][:3]+Lowell_SN_2+file.split('/')[len(file.split('/'))-1][3:]   
+    fname=file.split('/')[len(file.split('/'))-1]
+    if len(fname.split('_')[1])==2:# if the serieal number is only 2 digits make it 4
+        new_fname=fname[:3]+Lowell_SN_2+fname[3:]   
     else:
-        new_file=file.split('/')[len(file.split('/'))-1]
+        new_fname=fname
     
     #find # header rows through find the number of header lines in this file assuming "Depth"
     original_file=pd.read_csv(file,nrows=12,names=['0','1','2','3','4','5'])
     for i in range(len(original_file['0'])):
-        if original_file['0'][i]=='Depth':
-            header_rows=i  
+        if original_file['0'][i]=='HEADING':
+            header_rows=i
+            break   
+        
     # now, read header and data     
-    df=pd.read_csv(file,sep=',',skiprows=header_rows+1).dropna(axis=1,how='all') #data
+    df=pd.read_csv(file,sep=',',skiprows=header_rows).dropna(axis=1,how='all') #data
     name=['key','value','value1','value2','value3','value4']
-    df_head=pd.read_csv(file,sep=',',nrows=header_rows,names=name).dropna(axis=1,how='all') # header only
-
-    #the standard data have 6 columns, sometimes the data possible lack of the column of the HEADING. If lack, fixed it
+    df_head=pd.read_csv(file,sep=',',nrows=header_rows,names=name).dropna(axis=1,how='all')  # header only
+    #the standard data have 6 columns, sometimes the data possible lack of the column of the HEADING.If lack, fixed it
     if len(df.iloc[0])==5: # some files didn't have the "DATA" in the first column
         df.insert(0,'HEADING','DATA')
     #keep the lat and lon data format is right,such as 00000.0000w to 0000.0000
@@ -54,47 +71,89 @@ for file in file_lists:
         if len(str(df['lon'][i]).split('.')[0])>4 or 'A'<=str(df['lon'][i]).split('.')[1][len(str(df['lon'][i]).split('.')[1])-1:]<='Z':
             df['lon'][i]=str(df['lon'][i]).split('.')[0][len(str(df['lon'][i]).split('.')[0])-4:]+'.'+str(df['lon'][i]).split('.')[1][:4]
     
-    #check the data whether is the shoreside,if not,fix the vessel number
+    #check the header file whether exist or right,if not,repair it
+    header_file_fixed_type=['Date Format','Time Format','Temperature','Depth'] 
+    header_file_fixed_lowell=['YYYY-MM-DD','HH24:MI:SS','C','m']
+    loc=0
+    EXIST=0
+    for fixed_t in header_file_fixed_type:
+        for k in range(len(df_head['key'])):
+            if fixed_t.lower()==df_head['key'][k].lower():
+                break
+            else:
+                EXIST=1
+                count=k+1
+        if EXIST==1:
+            df_head=pd.concat([df_head[:count],pd.DataFrame(data=[[fixed_t,header_file_fixed_lowell[loc]]],columns=['key','value'])],ignore_index=True)
+        loc=loc+1 
+    #check if the data is the test data; Is the vessel number right?(test data's vessel number is 99)
     count=0
-    for i in range(len(df['Depth (m)'])):
-        count=count+(df['Depth (m)'][i]>mindepth)# keep track of # of depths>10
+    for i in range(len(df['Depth (m)'])):  #the value of count is 0 if the data is test data
+        count=count+(df['Depth (m)'][i]>mindepth)# keep track of # of depths>mindepth
+    vessel_name=fpath.split('/')[len(fpath.split('/'))-1:][0] #get the vessel name
     for j in range(len(df_head['value'])):
-        if df_head['key'][j]=='Vessel Number' or df_head['key'][j]=='vessel number':
-            LOC_V_number=j               
-            if count!=0:       #if data is shoreside,the number is 99;if not through the vessel_number to repaire it 
+        if df_head['key'][j].lower()=='Vessel Number'.lower():
+            LOC_V_number=j
+             #check and fix the vessel number              
+            if count!=0:      
                 for i in range(len(vessel_number_df['vessel_number'])):
                     if vessel_number_df['name'][i]==vessel_name:
-                        df_head['value'][j]=vessel_number_df['vessel_number'][i]
-            else:
-                df_head['value'][j]='99'
+                        df_head['value'][j]=str(vessel_number_df['vessel_number'][i])
+                    else:
+                        df_head['value'][j]=str(df_head['value'][j])
+            else:   
+                df_head['value'][j]='99' #the value of the vessel number is 99 if the data is test data
             break
-    
-    #check the vessel name whether exist or right,if not,repair it
-    EXIST=0   
-    #if the  vessel name s exist, find the location    
+        
+    #caculate the number of every vessel and boat file
+    for i in range(len(total_df['name'])):
+        if total_df['name'][i].lower()==vessel_name.lower():
+            total_df['file_total'][i]=total_df['file_total'][i]+1
+
+    #if the vessel name and serial number are exist, find the location of them 
+    EXIST=0 
+    S_number=0  
     for k in range(len(df_head['key'])):           
-        if df_head['key'][k]=='Vessel Name' or df_head['key'][k]=='vessel name':
+        if df_head['key'][k].lower()=='Vessel Name'.lower():
             EXIST=1
             LOC_V_NAME=k
-            break
-    #fix the vessel name 
-    
+        if df_head['key'][k].lower()=='Serial Number'.lower():
+            S_number=1
+            loc_S_number=k
+    #check and fix the vessel name and serial number 
+    if S_number==0:
+        df_head=pd.concat([df_head[:1],pd.DataFrame(data=[['Serial Number',new_fname.split('_')[1]]],columns=['key','value']),df_head[1:]],ignore_index=True)
+        LOC_V_NAME=LOC_V_NAME+1
+    if EXIST==0:
+        df_head=pd.concat([df_head[:2],pd.DataFrame(data=[['Vessel Name',vessel_name]],columns=['key','value']),df_head[2:]],ignore_index=True)
     if EXIST==1:
         df_head['value'][LOC_V_NAME]=vessel_name
-        new_head=df_head
-    else:
-        
-        new_head=pd.concat([df_head[:LOC_V_number+1],pd.DataFrame(data=[['Vessel Name',vessel_name]],columns=['Probe Type','Lowell']),df_head[LOC_V_number+1:]],ignore_index=True)
+    if S_number==1:
+        if len(df_head['value'][loc_S_number].split(':'))>1:
+            df_head['value'][loc_S_number]=df_head['value'][loc_S_number].replace(':','')
+    new_head=df_head
     if new_head['value'][LOC_V_number]=='99':
         new_head=new_head.replace(vessel_name,'Test')
-        print file       #if the file is test file,print it
-      
+        print file     #if the file is test file,print it
+        
     #creat the path and name of the new_file and the temperature file  
-    output_path_name=file.replace(input_dir2,'output_data/data/'+vessel_name+'/').replace(file.split('/')[len(file.split('/'))-1],new_file)#the output path and the print file. 
-    path_tem_file=input_dir+'df_tem.csv'
-    new_head.to_csv(output_path_name,header=0,index=0)
-    df.to_csv(path_tem_file,index=0)
-   
-   #add the two file in one file
-    os.system('cat '+path_tem_file+' >> '+output_path_name)
-    os.remove(path_tem_file)
+    output_path=fpath.replace(input_dir,output_dir)
+    if not os.path.exists(output_path):   #check the path of the save file is exist,make it if not
+        os.makedirs(output_path)
+    df.columns = ['HEADING','Datet(GMT)','Lat','Lon','Temperature(C)','Depth(m)']  #rename the name of conlum of data
+    path_tem_file=input_dir+'/df_tem.csv'
+    new_head.to_csv(output_path+'/'+new_fname,index=0,header=0)
+    df['Depth(m)'] = df['Depth(m)'].map(lambda x: '{0:.2f}'.format(float(x)))  #keep two decimal fraction
+    df['Temperature(C)'] = df['Temperature(C)'].map(lambda x: '{0:.2f}'.format(float(x)))
+    df['Lon'] = df['Lon'].map(lambda x: '{0:.4f}'.format(float(x)))
+    df['Lat'] = df['Lat'].map(lambda x: '{0:.4f}'.format(float(x)))#keep four decimal fraction
+    df.to_csv(output_path+'/df_tem.csv',index=0)  #produce the temperature file
+    
+   #add the two file in one file and delet the temperature file
+    os.system('cat '+output_path+'/df_tem.csv'+' >> '+output_path+'/'+new_fname)
+    os.remove(output_path+'/df_tem.csv')
+    
+#caculate the total of all files and print save as a file.
+for i in range(len(total_df['file_total'])):
+    total_df['file_total'][0]=total_df['file_total'][0]+total_df['file_total'][i]
+total_df.to_csv(output_dir+'/items_number.txt',index=0)
