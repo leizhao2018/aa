f73c2fede21245d43abe148b606418dc7305d54b
leizhao2018 <43003916+leizhao2018@users.noreply.github.com>
2018-12-14 13:37:47 -0500

    Update match_data.py

--- match_data.py
+++ match_data.py
@@ -20,7 +20,7 @@ telemetry_status='/home/jmanning/leizhao/data_file/telemetry_status - fitted .cs
 ######################
 
 #if input directory is not empty, it will exit.
-if os.listdir(output_dir):
+if os.listdir(input_dir):
     print ('please input a empty directory!')
     sys.exit()
 #read the file of the telementry_status
@@ -40,9 +40,10 @@ file_lists=glob.glob(os.path.join(input_dir,'*.csv'))
 #match the file   
 for file in file_lists:
     #time conversion, GMT time to local time
-    date=file.split('/')[len(file.split('/'))-1:][0].split('.')[0].split('_')[2] # get the gmt date,that date we get the current file
-    time=file.split('/')[len(file.split('/'))-1:][0].split('.')[0].split('_')[3] #get the gmt_time of current file,that time we start get the data
-    date_date=zl.gmt_to_eastern(date[0:4]+'-'+date[4:6]+'-'+date[6:8]+' '+time[0:2]+':'+time[2:4]+':'+time[4:6]).strftime("%Y%m%d") #get the local date that date we get the current file
+    time_str=file.split('/')[len(file.split('/'))-1:][0].split('.')[0].split('_')[2]+' '+file.split('/')[len(file.split('/'))-1:][0].split('.')[0].split('_')[3]
+    #GMT time to local time of file
+    time_local=zl.gmt_to_eastern(time_str[0:4]+'-'+time_str[4:6]+'-'+time_str[6:8]+' '+time_str[9:11]+':'+time_str[11:13]+':'+time_str[13:15]).strftime("%Y%m%d")
+
     #math the SN and date
     for i in range(len(telemetry_status_df)):
         if not telemetry_status_df['Lowell-SN'].isnull()[i] and not telemetry_status_df['logger_change'].isnull()[i]: #we will enter the next line if SN or date is not exist 
@@ -54,8 +55,8 @@ for file in file_lists:
                     dstfile=(fpath).replace(input_dir,output_dir+'/'+telemetry_status_df['Boat'][i]+'/'+fname) #produce the path+filename of the destination
                     #copy the file to the destination folder
                     if j<len(telemetry_status_df['logger_change'][i])-1:
-                        if telemetry_status_df['logger_change'][i][j]<=date_date<=telemetry_status_df['logger_change'][i][j+1]:
+                        if telemetry_status_df['logger_change'][i][j]<=time_local<=telemetry_status_df['logger_change'][i][j+1]:
                             zl.copyfile(file,dstfile)  
                     else:
-                        if telemetry_status_df['logger_change'][i][j]<=date_date:
+                        if telemetry_status_df['logger_change'][i][j]<=time_local:
                             zl.copyfile(file,dstfile) 
